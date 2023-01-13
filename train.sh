1458c738614526bed89ad4b42b440e73b75cf96b
Abay Bektursun <abaybektursun@gmail.com>
2023-01-12 18:29:54 -0600

    Delete train.sh

--- train.sh
+++
@@ -1,4 +0,0 @@
-DS=/home/abaybektursun/chart-gen/generated_stacked_bar/jsonl
-/app/donut/venv/bin/python train.py --config config/train_cord.yaml \
-                --dataset_name_or_paths '["./jsonl"]' \
-                --exp_version "abay_donut_28"    
