21e8a173dafc065fe4ffefdfb1155f752d917493
lyakaap <bepemgdlp@gmail.com>
2023-02-13 19:55:13 +0900

    add requirements

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     ),
     python_requires=">=3.7",
     install_requires=[
-        "transformers>=4.11.3",
+        "transformers>=4.25.1",
         "timm",
         "datasets[vision]",
         "pytorch-lightning>=1.6.4",
@@ -58,6 +58,9 @@ setup(
         "sentencepiece",
         "zss",
         "sconf>=0.2.3",
+        "scikit-learn",
+        "opencv-python~=4.5.0",
+        "fugashi[unidic-lite]~=1.2.1",
     ],
     classifiers=[
         "Intended Audience :: Developers",
