c5f55d612b72c9807db6592b123bffb2ed9718bb
lyakaap <bepemgdlp@gmail.com>
2023-02-13 20:05:29 +0900

    fix transformers version setting

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     ),
     python_requires=">=3.7",
     install_requires=[
-        "transformers>=4.25.1",
+        "transformers~=4.25.1",
         "timm",
         "datasets[vision]",
         "pytorch-lightning>=1.6.4",
