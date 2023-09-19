31d6086c13a9800650158d8ab5b0fe9dd52bcc6b
Max Berrendorf <max.berrendorf@gmail.com>
2023-09-19 14:24:48 +0200

    fix timm version

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     python_requires=">=3.7",
     install_requires=[
         "transformers>=4.11.3",
-        "timm",
+        "timm==0.5.4",
         "datasets[vision]",
         "pytorch-lightning>=1.6.4",
         "nltk",
